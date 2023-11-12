f2b7baaf1fe614053832f43af4a38ac4351f9f65
IrinaPapulova <142151842+IrinaPapulova@users.noreply.github.com>
2023-11-12 18:13:49 +0200

    task-1-3

---
+++ js/task-2.js
@@ -0,0 +1,13 @@
+function makeArray(firstArray, secondArray, maxLength) {
+        const CommonArr = firstArray.concat(secondArray);
+        const CommonArrCor = CommonArr.length > maxLength ? CommonArr.slice(0, maxLength) : CommonArr;
+
+        return CommonArrCor;     
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
